81023728682206bfd0004d50c2fa1fc33b7fee96
宋俊红 <Juny_Song@songjunhongdemacbook-pro.local>
2016-07-20 15:49:54 +0800

    Initial Commit

---
+++ SingleTonModel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SingleTonModel
+//
+//  Created by 宋俊红 on 16/7/20.
+//  Copyright © 2016年 Juny_song. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SingleTonModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SingleTonModel
+//
+//  Created by 宋俊红 on 16/7/20.
+//  Copyright © 2016年 Juny_song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
